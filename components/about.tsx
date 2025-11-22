40c2f3ecf48a38374f3ee752a105760d6b2541a8
ake144 <tamiratgedi1@gmail.com>
2025-11-22 12:24:13 +0300

    Enhance AboutMe component with PixelatedCanvas and improve layout structure

--- components/about.tsx
+++ components/about.tsx
@@ -24,20 +24,10 @@ const AboutMe = () => {
                     strokeDasharray={"4 2"}
                 /> */}
       <div className="text-3xl w-full flex justify-items-start items-center gap-12">
-        <h2 className="mb-4 w-1/3 text-4xl font-bold leading-tight tracking-tighter text-gray-900 dark:text-gray-100 md:text-3xl sm:text-2xl">
+        <div className="flex flex-col">
+           <h2 className="mb-4 w-1/3 text-4xl font-bold leading-tight tracking-tighter text-gray-900 dark:text-gray-100 md:text-3xl sm:text-2xl">
           About Me
         </h2>
-        <p className="max-w-4xl w-2/3  ml-12 justify-end items-end flex text-lg leading-7 text-gray-700 dark:text-gray-300">
-          Hello! I'm Aklilu Tamirat, a passionate developer with a love for
-          creating innovative solutions. With expertise in Javascript, and it's frameworks like React, Next.js, Node.js,Express, and Python, and other technologies. I
-          enjoy building applications that make a difference and involve in a day-to-day problem solving. When I'm not
-          coding, you can find me exploring new technologies or indulging in my
-          hobbies like hiking, reading, exploring nature and trying to do some research . Let's connect and create something
-          amazing together!
-        </p>
-      </div>
-      <div className="mt-12 flex flex-col-reverse lg:flex-row items-start justify-between w-full gap-8">
-        <div className="flex-1">
           <PixelatedCanvas
             src="/avatar.jpg"
             width={400}
@@ -59,7 +49,50 @@ const AboutMe = () => {
             tintStrength={0.2}
             className="rounded-xl border border-neutral-800 shadow-lg w-full max-w-md"
           />
+
         </div>
+       
+
+        <div className="flex-1 max-w-4xl w-2/3  ">
+
+        <p className=" ml-12 justify-end items-end flex text-lg leading-7 text-gray-700 dark:text-gray-300">
+          Hello! I'm Aklilu Tamirat, a passionate developer with a love for
+          creating innovative solutions. With expertise in Javascript, and it's frameworks like React, Next.js, Node.js,Express, and Python, and other technologies. I
+          enjoy building applications that make a difference and involve in a day-to-day problem solving. When I'm not
+          coding, you can find me exploring new technologies or indulging in my
+          hobbies like hiking, reading, exploring nature and trying to do some research . Let's connect and create something
+          amazing together!
+        
+        </p>
+          <p className="mt-6 justify-end text-lg w-2/3  text-gray-700 dark:text-gray-300 whitespace-nowrap">
+            I'm currently a software developer at <Link href="https://www.linkedin.com/company/african-fintech-hub/" className="underline">African Fintech Hub</Link> where I work on building scalable web applications and improving user experiences.
+          </p>
+      </div>
+      </div>
+      <div className="mt-12 flex flex-col-reverse lg:flex-row items-start justify-between w-full gap-8">
+       
+          {/* <PixelatedCanvas
+            src="/avatar.jpg"
+            width={400}
+            height={500}
+            cellSize={3}
+            dotScale={0.9}
+            shape="square"
+            backgroundColor="#000000"
+            dropoutStrength={0.4}
+            interactive
+            distortionStrength={3}
+            distortionRadius={80}
+            distortionMode="swirl"
+            followSpeed={0.2}
+            jitterStrength={4}
+            jitterSpeed={4}
+            sampleAverage
+            tintColor="#FFFFFF"
+            tintStrength={0.2}
+            className="rounded-xl border border-neutral-800 shadow-lg w-full max-w-md"
+          /> */}
+      
 
         <aside className="w-full lg:w-96">
           <div className="rounded-2xl bg-white/60 dark:bg-black/60 backdrop-blur-md border border-gray-200 dark:border-neutral-800 p-6 shadow-xl">
--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -4,14 +4,14 @@ import AboutMe from "./about";
 
 const HeroSection=()=>{
     return (
-<div className="flex min-h-screen max-w-4xl w-full items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+<div className="flex  max-w-4xl w-full items-center justify-center bg-zinc-50 font-sans dark:bg-black">
    
 
-      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-auto w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
            
      <div
         className={cn(
-          "absolute inset-0 z-0 px-22 mx-76  flex justify-center items-center ",
+          "absolute inset-0 z-0 px-22 mx-76  min-h-auto h-screen flex justify-center items-center ",
           "[background-size:40px_40px]",
           "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
           "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
